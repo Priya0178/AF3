b47332edcc1937014a88e13a2c8969ec0dc53c8b
ｒｙｍｅ <ryme.care@hotmail.com>
2023-08-02 22:32:44 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -30,7 +30,7 @@ BUTTONS = {}
 SPELL_CHECK = {}
 
 @Client.on_message(filters.private & filters.text & filters.incoming)
-async def private_filter(client, message):
+async def give_private_filter(client, message):
     await manual_filters(client, message)
 
 
