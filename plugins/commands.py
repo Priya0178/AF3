434d49606e5502e52dc78debfd802f8970aed040
Mуѕтєяισυѕ gιяℓ <102360292+ItzQueen-TG@users.noreply.github.com>
2023-08-03 18:26:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,13 +42,12 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('🎬 Rᴇqᴜᴇꜱᴛ Mᴏᴠɪᴇ', callback_data='blahs'),
+            InlineKeyboardButton('Sᴇᴀʀᴄʜ Mᴏᴠɪᴇ 🎭', switch_inline_query_current_chat='')
+        ], [
+            InlineKeyboardButton('⚙️ Bᴏᴛ Uᴩᴅᴀᴛᴇꜱ Cʜᴀɴɴᴇʟ ⚙️', url='https://t.me/+xLg-GrBkHcJmMmM1')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
-            ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('💻 Oᴛᴛ Mᴏᴠɪᴇ Uᴩᴅᴀᴛᴇꜱ 💻', url='https://t.me/+CS7ywuF7ZeA3YWJl')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -92,13 +91,12 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('🎬 Rᴇqᴜᴇꜱᴛ Mᴏᴠɪᴇ', callback_data='blahs'),
+            InlineKeyboardButton('Sᴇᴀʀᴄʜ Mᴏᴠɪᴇ 🎭', switch_inline_query_current_chat='')
+        ], [
+            InlineKeyboardButton('⚙️ Bᴏᴛ Uᴩᴅᴀᴛᴇꜱ Cʜᴀɴɴᴇʟ ⚙️', url='https://t.me/+xLg-GrBkHcJmMmM1')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
-            ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('💻 Oᴛᴛ Mᴏᴠɪᴇ Uᴩᴅᴀᴛᴇꜱ 💻', url='https://t.me/+CS7ywuF7ZeA3YWJl')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
