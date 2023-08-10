1631bba8edd5899cf8ecd80893da006316dfde80
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2023-08-10 14:18:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -72,7 +72,7 @@ async def next_page(bot, query):
             [
                 InlineKeyboardButton(
                     text=f"[{get_size(file.file_size)}] {{re.sub(r'\\[.*?\\]\\s*', '', file.file_name)}}",
-                    callback_data=f'files#{file.file_id}'
+                    callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
