d1b793aa0f91a46f15dd4dae3928495c53458219
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2023-08-16 09:39:46 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -635,7 +635,7 @@ async def auto_filter(client, msg, spoll=False):
         if 2 < len(message.text) < 100:
             search = message.text
             files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
-            if not files
+            if not files:
                 xxx = search.replace(" ", "+")
                 ntn = [
                     [
