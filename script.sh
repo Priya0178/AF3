3b7bca75657b5db0b2837f45491610dcf1907f27
SONU KANWAR <111267360+Priya0178@users.noreply.github.com>
2023-11-25 22:22:53 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+
 pip install -r requirements.txt
+
 while true; do
     script_name="bot.py"
-    
-    # Run the Python script
-    python3 "$script_name" &
-    # Sleep for 2 minutes before running the script again
+
+    # Run the Python script in the foreground
+    python3 "$script_name"
+
+    # Sleep for 2 minutes
     sleep 120
+
+    # Get the process ID of the existing bot.py process
     existing_pid=$(pgrep -f "$script_name")
 
     if [ -n "$existing_pid" ]; then
