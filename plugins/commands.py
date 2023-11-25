2713ac937779144872573d4a987a36c5a119e20f
SONU KANWAR <111267360+Priya0178@users.noreply.github.com>
2023-11-25 13:56:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -48,11 +48,15 @@ async def start(client, message):
             InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
+        try:
+            await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
+        except Exception as e:
+            print(e)
+            
         )
         return
     if AUTH_CHANNEL and not await is_subscribed(client, message):
