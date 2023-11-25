d0c888258df6b2af8673503c97b3e04cbbf9be66
SONU KANWAR <111267360+Priya0178@users.noreply.github.com>
2023-11-25 14:00:49 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -54,7 +54,7 @@ async def start(client, message):
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
-        except Exception as e:
+        except error as e:
             print(e)
             
         )
