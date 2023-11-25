4c90cc726cd0a14de1577dc38593b1412a2168b5
SONU KANWAR <111267360+Priya0178@users.noreply.github.com>
2023-11-25 22:51:28 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
-
+git pull
 pip install -r requirements.txt
+script_name="bot.py"
+
+start_bot() {
+    echo "Starting $script_name..."
+    python3 "$script_name" &
+}
+
+stop_bot() {
+    # Get the process ID of the existing bot.py process
+    existing_pid=$(pgrep -f "$script_name")
+
+    if [ -n "$existing_pid" ]; then
+        echo "Killing existing $script_name process with PID $existing_pid"
+        kill "$existing_pid"
+    fi
+}
 
 while true; do
-    script_name="bot.py"
+    # Start the bot
+    start_bot
 
-    # Run the Python script with a timeout of 2 minutes
-    timeout 2m python3 "$script_name"
+    sleep 120
 
-    # Sleep for a short duration before restarting (adjust as needed)
-    sleep 2
+    stop_bot
 done
