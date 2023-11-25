3c9e02012a9c46b7e81aa7f9076fa76cb04e465d
SONU KANWAR <111267360+Priya0178@users.noreply.github.com>
2023-11-26 00:06:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -80,7 +80,7 @@ signal.signal(signal.SIGTERM, signal_handler)
 
 async def main():
     try:
-        app.run()
+        await app.run()
 
     except KeyboardInterrupt:
         logging.info("Bot interrupted by user.")
