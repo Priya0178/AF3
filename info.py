8b58a155415d0c7022865fcb09354cb0ca5225bd
SONU KANWAR <111267360+Priya0178@users.noreply.github.com>
2023-11-25 12:49:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,11 +32,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-<<<<<<< HEAD
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://priyajat427:YltKDwmzpRJEIuZ8@cluster0.mchxxap.mongodb.net/?retryWrites=true&w=majority")
-=======
-DATABASE_URI = environ.get('DATABASE_URI', "")
->>>>>>> da093a0e14e2e676375a1cef14bec446e9a56d20
+
 DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
