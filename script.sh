f5c89e14f73ea607ac7d63dd7f37ef2c886a91c6
SONU KANWAR <111267360+Priya0178@users.noreply.github.com>
2023-11-25 22:29:19 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,18 +5,9 @@ pip install -r requirements.txt
 while true; do
     script_name="bot.py"
 
-    # Run the Python script in the foreground
-    python3 "$script_name"
+    # Run the Python script with a timeout of 2 minutes
+    timeout 2m python3 "$script_name"
 
-    # Sleep for 2 minutes
-    sleep 120
-
-    # Get the process ID of the existing bot.py process
-    existing_pid=$(pgrep -f "$script_name")
-
-        if [ -n "$existing_pid" ]; then
-            # Kill the existing bot.py process
-            echo "Killing existing bot.py process with PID $existing_pid"
-            kill "$existing_pid"
-    fi
+    # Sleep for a short duration before restarting (adjust as needed)
+    sleep 2
 done
