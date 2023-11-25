d7221f873548bb5f45f9608ab24dd2c95c694de0
SONU KANWAR <111267360+Priya0178@users.noreply.github.com>
2023-11-25 22:18:33 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+pip install -r requirements.txt
+while true; do
+    script_name="bot.py"
+    
+    # Run the Python script
+    python3 "$script_name" &
+    existing_pid=$(pgrep -f "$script_name")
+
+    if [ -n "$existing_pid" ]; then
+        # Kill the existing bot.py process
+        echo "Killing existing bot.py process with PID $existing_pid"
+        kill "$existing_pid"
+    fi
+    # Sleep for 2 minutes before running the script again
+    sleep 120
+done
