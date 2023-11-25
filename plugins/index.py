1df9bb64de6caab89f7be1a2a2cc67f82706679c
SONU KANWAR <111267360+Priya0178@users.noreply.github.com>
2023-11-25 14:53:45 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -67,7 +67,7 @@ async def send_for_index(bot, message):
         return
     try:
         await bot.get_chat(chat_id)
-    except ChannelInvalid,ChannelPrivate:
+    except ChannelPrivate:
         return await message.reply('This may be a private channel / group. Make me an admin over there to index the files.')
     except (UsernameInvalid, UsernameNotModified):
         return await message.reply('Invalid Link specified.')
