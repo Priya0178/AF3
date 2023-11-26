129fdcd935dbb2ad35bad426d13a21960f63811a
SONU KANWAR <111267360+Priya0178@users.noreply.github.com>
2023-11-27 00:30:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,9 +33,7 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            workers=10,
-            plugins={"root": "plugins"},
-            sleep_threshold=10,
+            plugins={"root": "plugins"}
         )
 
     async def start_bot(self):
