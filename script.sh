594a3c4d3213d7d800027422f753196344ef9543
SONU KANWAR <111267360+Priya0178@users.noreply.github.com>
2023-11-26 10:40:21 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,26 +6,22 @@ update() {
     pip install -r requirements.txt
     script_name="bot.py"
 }
+
 start_bot() {
     echo "Starting $script_name..."
     python3 "$script_name" &
 }
 
 stop_bot() {
-    # Get the process ID of the existing bot.py process
-    existing_pid=$(pgrep -f "$script_name")
-
-    if [ -n "$existing_pid" ]; then
-        echo "Killing existing $script_name process with PID $existing_pid"
-        kill "$existing_pid"
-    fi
+    # Use pkill to send SIGTERM to the process with the specified name
+    pkill -f "$script_name"
 }
 
 while true; do
     update
-    sleep 10
     start_bot
 
+    # Sleep for 2 minutes
     sleep 120
 
     stop_bot
