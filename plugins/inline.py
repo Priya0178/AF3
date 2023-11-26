45c55b9e8181f952c5f7e299fac9bdc91bdda286
SONU KANWAR <111267360+Priya0178@users.noreply.github.com>
2023-11-26 12:15:13 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -76,7 +76,7 @@ async def answer(bot, query):
         InlineQueryResultCachedDocument(
                 title=file.file_name,
                 document_file_id=file.file_id,
-                caption=f_caption,
+                caption=batman,
                 description=f'Size: {get_size(file.file_size)}\nType: {file.file_type}',
                 reply_markup=reply_markup))
 
